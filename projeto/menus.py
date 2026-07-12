2c1ca3917cf1a5050742845aef04678f6911b28c
Moreiiirax <moreira.tiago07@aluno.ifce.edu.br>
2026-07-12 14:25:20 -0300

    Criação do Modulo Menus
    Criei o modulo menus, com os menus que irei utilizar no projeto

---
+++ projeto/menus.py
@@ -0,0 +1,18 @@
+def menu_funcao():
+    print("=" * 50)
+    print("\tMini System")
+    print("1 - Cadastrar \n2 - Editar \n3 - Listar \n4 - Excluir \n0 - Sair")
+    print("=" * 50)
+
+
+def menu_instituto():
+    print("=" * 50)
+    print("\tMini System")
+    print("1 - Proprietário \n2 - Imóvel \n3 - Inquilino \n4 - Contrato \n0 - Voltar ao menu anterior")
+    print("=" * 50)
+
+
+def menu_cabecalho():
+    print("=" * 50)
+    print("\tMini System")
+    print("=" * 50)
