52088b015f04ab350d2b047683e11ee7955118d6
Moreiiirax <moreira.tiago07@aluno.ifce.edu.br>
2026-07-15 11:45:56 -0300

    Update menus.py
    Otimizei as funções e criei um menu para a funcao editar

--- projeto/menus.py
+++ projeto/menus.py
@@ -1,20 +1,20 @@
-def menu_funcao():
+def menu_cabecalho():
     print("=" * 50)
     print("\t\t  Mini System")
     print("=" * 50)
+    
+def menu_funcao():
+    menu_cabecalho()
     print("1 - Cadastrar \n2 - Listar \n3 - Excluir \n4 - Editar \n0 - Sair")
     print("=" * 50)
 
 
 def menu_instituto():
-    print("=" * 50)
-    print("\t\t  Mini System")
-    print("=" * 50)
-    print("1 - Proprietário \n2 - Imóvel \n3 - Inquilino \n4 - Contrato \n0 - Voltar ao menu anterior")
+   menu_cabecalho()
+    print("1 - Proprietário \n2 - Imóvel \n3 - Inquilino \n4 - Contrato \n0 - Voltar ao Menu Anterior")
     print("=" * 50)
 
-
-def menu_cabecalho():
-    print("=" * 50)
-    print("\t\t  Mini System")
+def menu_editar():
+    menu_cabecalho()
+    print("1 - Id \n2 - Nome \n3 - CPF \n4 - Editar \n0 - Voltar ao Menu Anterior")
     print("=" * 50)
